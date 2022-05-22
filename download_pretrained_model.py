d052d2ccaded35e653ef8fd1ee8a651208adeea5
TvanTreeck <104461490+TvanTreeck@users.noreply.github.com>
2022-05-23 01:36:47 +0200

    fix

--- download_pretrained_model.py
+++ download_pretrained_model.py
@@ -1,25 +1,28 @@
 import torch
+import os
 from pytorch_pretrained_gans.BigGAN import make_biggan
 import matplotlib.pyplot as plt
 
 # Sample a class-conditional image from BigGAN with default resolution 256
 gan_type='biggan-deep-128'
-G = make_biggan(model_name=gan_type)
 
-state_dict  = G.G.state_dict()
-config = G.G.config
+if not os.path.isfile("models/"+gan_type+"_pretrained_model.pth.tar"):
+    G = make_biggan(model_name=gan_type)
 
-torch.save(state_dict, "models/"+gan_type+"_pretrained_model.pth.tar")
-             
-with open(gan_type+"_config.json", "w") as f:
-    f.write(config.to_json_string())
-             
+    state_dict  = G.G.state_dict()
+    config = G.G.config
 
-for i in range(5):
-    y = G.sample_class(batch_size=1)  # -> torch.Size([1, 1000])
-    z = G.sample_latent(batch_size=1)  # -> torch.Size([1, 128])
-    x = G(z=z, y=y)  
+    torch.save(state_dict, "models/"+gan_type+"_pretrained_model.pth.tar")
 
-    plt.figure()
-    plt.imshow(x.squeeze(0).transpose(1,0).transpose(2,1).detach().numpy())
-    plt.savefig("models/"+gan_type+f"pretrained_sample_{i}")
+    with open(gan_type+"_config.json", "w") as f:
+        f.write(config.to_json_string())
+
+
+    for i in range(5):
+        y = G.sample_class(batch_size=1)  # -> torch.Size([1, 1000])
+        z = G.sample_latent(batch_size=1)  # -> torch.Size([1, 128])
+        x = G(z=z, y=y)
+
+        plt.figure()
+        plt.imshow(x.squeeze(0).transpose(1,0).transpose(2,1).detach().numpy())
+        plt.savefig("models/"+gan_type+f"pretrained_sample_{i}")
