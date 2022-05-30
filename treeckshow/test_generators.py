69f20090410426f96c7f8e6b7b0712c52b11e9b0
TvanTreeck <104461490+TvanTreeck@users.noreply.github.com>
2022-05-30 22:06:41 +0200

    fix

--- treeckshow/test_generators.py
+++ treeckshow/test_generators.py
@@ -14,17 +14,17 @@ def init_inference(class_index, model_path, plot_path):
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Test Generators')
-    parser.add_argument('--path', type=str, default="models", help='path to checkpoints')
+    parser.add_argument('--model_path', type=str, default="models", help='path to checkpoints')
     parser.add_argument('--plot_path', type=str, default="generator_samples", help='path to ouput images')
     parser.add_argument('--class_index', type=str, default="1", help='class_index')
     args = parser.parse_args()
 
-    files = os.listdir(args.path)
+    files = os.listdir(args.model_path)
     files = [f for f in files if "generator" in f and ".pt" in f]
 
     os.makedirs(args.plot_path, exist_ok=True)
     for i, f in enumerate(files):
         print(f, "progress",  i/len(files))
-        model_path = os.path.join(args.path, f)
+        model_path = os.path.join(args.model_path, f)
         image_path = os.path.join(args.plot_path, f.replace(".pt", ".png"))
         init_inference(class_index=args.class_index, model_path=args.model_path, plot_path=args.plot_path)
