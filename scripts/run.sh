1d05d625faacc73fa5d93806bd7afc6573f0ac88
TvanTreeck <104461490+TvanTreeck@users.noreply.github.com>
2022-06-05 18:39:07 +0200

    fix run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,17 +2,20 @@ python treeckshow/download_pretrained_model.py
 
 python treeckshow/prepare_data.py \
   --imgs_path="images" \
-  --img_size=128 \
-  --img_width=128 \
+  --img_size=256 \
+  --img_width=256 \
   --channels=3
 
 python treeckshow/generate_discriminator_features.py \
   --sample_mode="1" # sample für alle classen, classen index (zahl) für eine classe
 
-python treeckshow/train_discriminator.py
+python treeckshow/train_discriminator.py \
+  --img_size=256 \
+  --img_width=256 \
+  --channels=3
 
 python treeckshow/train.py \
   --sample_mode="1" \
-  --img_size=128 \
-  --img_width=128 \
+  --img_size=256 \
+  --img_width=256 \
   --channels=3 
